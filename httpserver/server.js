b9fe359488634b90a2d4362bc4f90cf538528b8b
sananvanshika <Vanshika.s@antino.io>
2023-07-24 22:31:56 +0530

    Created a handlerequest to create server and content type

--- httpserver/server.js
+++ httpserver/server.js
@@ -1,22 +1,25 @@
-// Import the http module
 const http = require("http");
 
-// server instance
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Default Route - Node Js Server Response Successful");
-  } else if (req.url === "/aboutus") {
-    res.end("This is About Us Page");
-  } else if (req.url === "/contact") {
-    res.end("This is Contact Page");
-  } else if (req.url === "/home") {
-    res.end("This is Home Page");
-  } else {
-    res.end("Error-404 Page Not Found");
-  }
-});
+const routes = {
+  "/": "Default Route - Node.js Server Response Successful",
+  "/aboutus": "This is About Us Page",
+  "/contact": "This is Contact Page",
+  "/home": "This is Home Page",
+};
 
-// specifying port no = 8000 and Ip address = 127.0.0.1 and start the server
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to http://localhost:8000");
+const handleRequest = (req, res) => {
+  const url = req.url;
+  const content = routes[url] || "Error 404 - Page Not Found";
+
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(content);
+};
+
+const server = http.createServer(handleRequest);
+
+const PORT = 8000;
+const IPADDRESS = "127.0.0.1";
+
+server.listen(PORT, IPADDRESS, () => {
+  console.log(`Server running at http://${IPADDRESS}:${PORT}/`);
 });
